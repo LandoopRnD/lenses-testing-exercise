4e1a7a2c7273ea7fbfb1de2d900889187fa6194a
Sebastian Dragomir <sebastian@dragomir.co.uk>
2019-05-27 14:43:30 +0200

    Minor fix due to change of label.

--- cypress/integration/lenses/dataprotection.spec.js
+++ cypress/integration/lenses/dataprotection.spec.js
@@ -1,16 +1,18 @@
 describe.only("Data Protection", () => {
   beforeEach(() => {
     cy.login();
-    cy.get('.navbar').should('exist');
+    cy.get(".navbar", { timeout: 10000 }).should("exist");
   });
 
   context("Creation", () => {
     it("I can navigate to the Policies page", () => {
-      cy.get('.navbar a[href="/dataprotection/policies"]').should('contain', 'POLICIES').click();
+      cy.get('.navbar a[href="/dataprotection/policies"]')
+        .should("contain", "POLICIES")
+        .click();
       cy.get("#dataProtectionContainer").should("exist");
       cy.get('button[class="btn btn-secondary btn-secondary-lenses"]').should(
         "contain",
-        "Create Policy"
+        "New policy"
       );
     });
   });
